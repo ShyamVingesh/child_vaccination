46110e569c0bf035bdd0e5476d295aca6f77e3ac
ShyamVingesh <shyamvignesh5@gmail.com>
2023-07-05 17:39:24 +0530

    child vaccination initial commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function Home() {
+  return (
+    <div
+    style={{
+      textAlign: "center",
+      paddingTop: "300px",
+    }}>
+     
+      <h1>Home</h1>
+      <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+    </div>
+    
+  );
+}
+
+export default Home;
